4775409baaa9f4d31a295c1a21b5ff9032dd5cae
Nasrolahi <snasrolahi93@gmail.com>
2019-09-05 14:48:08 +0330

    Some Changes

--- app/src/main/java/com/example/finaltask/MainActivity.java
+++ app/src/main/java/com/example/finaltask/MainActivity.java
@@ -1,10 +1,25 @@
 package com.example.finaltask;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView txtCityName;
+    TextView txtDate;
+    TextView txtCityTempreture;
+    ImageView imgIcon;
+    TextView txtHumidity;
+    RecyclerView recycler;
+    TextView txtWind;
+    EditText edtSearchCity;
+    Button btnSeachCity;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
